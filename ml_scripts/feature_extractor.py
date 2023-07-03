f83d91db47e243524c1cabdf7b70a9f48668bb0c
kuroazai <pyromwansa@gmail.com>
2023-07-03 21:23:52 +0100

    feature extraction

--- ml_scripts/feature_extractor.py
+++ ml_scripts/feature_extractor.py
@@ -1,23 +1,14 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 
-
 def extract_features(dataset):
-    # Extract input texts and corresponding labels from the dataset
-    texts = dataset['text']
-    labels = dataset['intent']
+    texts = dataset['text'].tolist()
+    labels = dataset['intent'].tolist()
 
-    # Initialize the CountVectorizer for bag-of-words representation
     vectorizer = CountVectorizer()
+    features = vectorizer.fit_transform(texts)
 
-    # Fit the vectorizer on the input texts to learn the vocabulary
-    vectorizer.fit(texts)
-
-    # Transform the input texts into a document-term matrix
-    features = vectorizer.transform(texts)
-
-    # Encode the intent labels as integers
     label_encoder = LabelEncoder()
-    encoded_labels = label_encoder.fit_transform(labels)
+    encoded_labels = label_encoder.fit_transform([label for labels in labels for label in labels])
 
     return features, encoded_labels, vectorizer, label_encoder
