3545e1e266a11c43ae33800fe67b2ce3b4fc9dfd
kuroazai <pyromwansa@gmail.com>
2023-07-04 13:13:17 +0100

    update feature extraction

--- ml_scripts/feature_extractor.py
+++ ml_scripts/feature_extractor.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MultiLabelBinarizer
+
 
 def extract_features(dataset):
     texts = dataset['text'].tolist()
@@ -8,7 +9,7 @@ def extract_features(dataset):
     vectorizer = CountVectorizer()
     features = vectorizer.fit_transform(texts)
 
-    label_encoder = LabelEncoder()
-    encoded_labels = label_encoder.fit_transform([label for labels in labels for label in labels])
+    label_encoder = MultiLabelBinarizer()
+    encoded_labels = label_encoder.fit_transform(labels)
 
     return features, encoded_labels, vectorizer, label_encoder
