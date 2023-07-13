e1f99069b2230c357a894127f699519bffd79da5
kuroazai <pyromwansa@gmail.com>
2023-07-13 23:52:11 +0100

    rest endpoints

--- app.py
+++ app.py
@@ -1,28 +1,23 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
-from databases.db_mongo import MongoDB
-
+from databases.redis_db import RedisConn
+import json
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/ff-tcg-fast-storage"
-app.config["JWT_SECRET_KEY"] = "your-secret-key"
 api = Api(app)
-Mongo = MongoDB(app.config["MONGO_URI"], "yugioh")
+redis = RedisConn()
+
 
 class GetAllCards(Resource):
     def get(self) -> list:
-        cards = Mongo.db.cards.find()
-
-        if not cards:
-            return {"message": "No cards found"}, 404
-
-        return jsonify([card for card in cards])
+        cards = json.loads(redis.get("ff-tcg-cards")).get("cards")
+        return jsonify(cards)
 
 
 class GetCard(Resource):
     def get(self):
         card_id = request.args.get('id')
-        card = Mongo.find_one("cards", {"_id": card_id})
+        card = json.loads(redis.get('ff-tcg-cards')).get(card_id)
         if card:
             return jsonify(card)
         return {"message": "Card not found"}, 404
@@ -31,15 +26,25 @@ class GetCard(Resource):
 class GetCardIntents(Resource):
     def get(self):
         card_id = request.args.get('id')
-        card = Mongo.find_one("cards", {"_id": card_id})
+        card = redis.get(card_id)
         if card:
             return jsonify(card['intents'])
         return {"message": "Card not found"}, 404
 
+
 class GetCardDescription(Resource):
     def get(self):
         card_id = request.args.get('id')
-        card = Mongo.find_one("cards", {"_id": card_id})
+        card = redis.get(card_id)
         if card:
-            return jsonify(card['description'])
+            return jsonify(card['text'])
         return {"message": "Card not found"}, 404
+
+
+api.add_resource(GetAllCards, "/cards")
+api.add_resource(GetCard, "/card")
+api.add_resource(GetCardIntents, "/card/intents")
+api.add_resource(GetCardDescription, "/card/description")
+
+if __name__ == "__main__":
+    app.run(debug=True)
